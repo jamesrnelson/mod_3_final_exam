6bfc05e1dd893a060750f185a3a6c56d2f58605f
JNelson <nelson.jimmy@gmail.com>
2018-05-30 10:51:03 -0600

    added test for sending api requests and started adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root "welcome#index"
   get '/validation', to: 'validation#index'
+
+  namespace :api do
+    namespace :v1 do
+      namespace :games do
+        
+      end
+    end
+  end
 end
---
+++ spec/requests/api/v1/user_can_start_game_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'api/v1/games/:id' do
+  context 'user sends get request' do
+    it 'should receive the appropriate response' do
+
+      get '/api/v1/games/1'
+
+      expect(response).to be_success
+
+      game = JSON.parse(response.body)
+
+      expect(game['game_id']).to eq(1)
+      expect(game['scores'].first['user_id']).to eq(1)
+      expect(game['scores'].first['score']).to eq(15)
+      expect(game['scores'].second['user_id']).to eq(2)
+      expect(game['scores'].second['score']).to eq(16)
+    end
+  end
+end
