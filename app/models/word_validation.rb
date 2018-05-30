230056182e3f09d9c2c5f6e10cc036ae13ab4fb6
JNelson <nelson.jimmy@gmail.com>
2018-05-30 09:40:11 -0600

    implementing code in WordValidation. Adding Faraday with headers.

--- app/models/word_validation.rb
+++ app/models/word_validation.rb
@@ -1,5 +1,20 @@
 class WordValidation
+  attr_reader :search
   def initialize(search)
     @search = search
+  end
+
+  def raw_search
+
+  def connection
+    Faraday.new(url: "https://od-api.oxforddictionaries.com:443/api/v1/inflections/en/#{search}")
+  end
+
+  def response
+    connection.get do |req|
+      req.headers['Accept'] = 'application/json'
+      req.headers['app_id'] = ENV['OXFORD_APPLICATION_ID']
+      req.headers['app_key'] = ENV['OXFORD_APPLICATION_KEY']
+    end
   end
 end
