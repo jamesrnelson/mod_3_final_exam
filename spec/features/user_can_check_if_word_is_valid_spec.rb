f4d4b1095a5c2a8560ca50279b407fd93ca5f951
JNelson <nelson.jimmy@gmail.com>
2018-05-30 09:22:40 -0600

    Added first feature test and wrote my first impressions of what the view will contain. Added the search bar to the welcome index and the validation controller.

---
+++ spec/features/user_can_check_if_word_is_valid_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Guest User' do
+  context 'visits root path and fills in word' do
+    it 'a valid word should be valid' do
+      visit root_path
+      # As a guest user (no sign in necessary)
+      # When I visit "/"
+
+      fill_in :search, with: 'foxes'
+      # And I fill in a text box with "foxes"
+      click_on 'Validate Word'
+      # And I click "Validate Word"
+      expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'.")
+      # Then I should see a message that says "'foxes' is a valid word and its root form is 'fox'."
+
+    end
+
+    it 'an invalid word should be invalid' do
+      visit root_path
+      # As a guest user
+      # When I visit "/"
+      fill_in :search, with: 'foxez'
+      # And I fill in a text box with "foxez"
+      click_on 'Validate Word'
+      # And I click "Validate Word"
+      expect(page).to have_content("'foxez' is not a valid word.")
+      # Then I should see a message that says "'foxez' is not a valid word."
+    end
+  end
+end
