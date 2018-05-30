7cc2cdaaf1b68f5a091e6c0e4898d009f5670992
JNelson <nelson.jimmy@gmail.com>
2018-05-30 09:29:37 -0600

    added methods to word validation test

--- spec/models/word_validation_spec.rb
+++ spec/models/word_validation_spec.rb
@@ -5,4 +5,14 @@ describe WordValidation do
   it 'exists' do
     expect(subject).to be_a WordValidation
   end
+
+  context 'instance methods' do
+    it '#word' do
+      expect(subject.word).to eq('foxes')
+    end
+
+    it '#root_form' do
+      expect(subject.root_form).to eq('fox')
+    end
+  end
 end
